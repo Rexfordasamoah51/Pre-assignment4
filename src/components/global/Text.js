2bcdff50f0a8b7ef1af6bbcfaf10beaebf8222da
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-03-25 13:40:10 +0000

    login form

---
+++ src/components/global/Text.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+
+export const BlackHeaderText = styled.h3`
+font-weight: bold;
+color: black;
+font-family: 'Lato', sans-serif;
+`
+
+export const BlackHeaderTextBigger = styled.h2`
+font-weight: bold;
+color: black;
+font-family: 'Lato', sans-serif;
+`
+
+export const LighterTextDeem = styled.p`
+color: black;
+font-weight: normal;
+margin-top:0px;
+color:#DADDDF;
+text-align: start;
+`
+export const BlackTextDeem = styled.p`
+margin-top:20px;
+color: black;
+font-weight: bold;
+text-align: start;
+`
